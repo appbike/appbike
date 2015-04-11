6e2f30182b84df378f19dc7ae10387e1070e57ee
Ashwin Jumani <ashwin.jumani@gmail.com>
2015-04-11 08:19:57 +0530

    Update new changes

---
+++ appbike/Classes/Controllers/ABDiagnosticVC.h
@@ -0,0 +1,13 @@
+//
+//  ABDiagnosticVC.h
+//  appbike
+//
+//  Created by Ashwin Jumani on 4/10/15.
+//  Copyright (c) 2015 Zaptech Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABDiagnosticVC : UIViewController
+
+@end
---
+++ appbike/Classes/Navigation/ABNavigationVC.h
@@ -0,0 +1,13 @@
+//
+//  ABNavigationVC.h
+//  appbike
+//
+//  Created by Ashwin Jumani on 4/10/15.
+//  Copyright (c) 2015 Zaptech Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ABNavigationVC : UINavigationController
+
+@end
